64ee7d3232b956b26d3b740371403ae823fb4533
Charles Wiltermood <cwiltermood@gmail.com>
2022-12-04 23:19:41 -0800

    another commit

--- pages/[id].js
+++ pages/[id].js
@@ -13,7 +13,7 @@ export async function getStaticProps({ params }) {
 }
 
 export async function getStaticPaths() {
-  const paths = getAllIds();
+  const paths = await getAllIds();
   return {
     paths,
     fallback: false
@@ -25,12 +25,9 @@ export default function Entry({ itemData }) {
      <Layout>
       <article className="card col-6">
         <div className="card-body">
-          <h5 className="card-title">{itemData.name}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">{itemData.phone}</h6>
-          <p className="card-text">{itemData.birthdate}</p>
-          <p className="card-text"> Allergies- {itemData.allergies}</p>
-           <p className="card-text"> Medical- {itemData.medical}</p>
-          <a href={'mailto:' + itemData.email} className="card-link">{itemData.email}</a>
+          <h5 className="card-title">{itemData.post_title}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">{itemData.user_login}</h6>
+        <div className="card-text" dangerouslySetInnerHTML={{__html: itemData.post_content }} />
         </div>
       </article>
        </Layout>
